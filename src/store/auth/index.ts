97179b968666d0295cb66bea7ea9f8373fb680e6
wade-goodoc <wade.kim@goodoc.co.kr>
2024-11-07 18:48:29 +0900

    store 누락 적용

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -4,3 +4,10 @@ export const loginStatus = atom<boolean>({
   key: 'loginStatus',
   default: false
 });
+
+export const therapistInfo = atom<{ sendbirdUserId: string }>({
+  key: 'therapistInfo',
+  default: {
+    sendbirdUserId: 'jackson.hong'
+  }
+});
